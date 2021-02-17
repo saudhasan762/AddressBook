75badea825a20a0594f8a9306c8ad994dab2508f
saudhasan762 <azanali152@gmail.com>
2021-02-17 13:02:46 +0530

    Added Interface

---
+++ Compute.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public interface Compute {
+	public static ArrayList<Contact> list = new ArrayList<>();
+	public void view();
+	
+	public boolean add(String checkname);
+		
+	public boolean edit(String first, String last, String address, String city, String state,String email, String zip, String phoneNumber );
+	
+	public boolean delete(String first,String last);
+
+}
